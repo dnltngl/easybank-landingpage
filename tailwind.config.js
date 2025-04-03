b7c81616b75cf9bd2b1dadf4ecb71d8d378f49ba
xjanus12x <albertovillacarlos07@gmail.com>
2025-04-03 20:58:35 +0800

    add card-themed color variables to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "card-author-foreground": "var(--gray-light)",
+        "card-title-foreground": "var(--blue-dark)",
+        "card-description-foreground": "var(--gray-light)",
+      },
+    },
   },
   plugins: [],
-}
+};
