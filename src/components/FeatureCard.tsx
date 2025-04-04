8f6d01860040660dc69e65124bfc037c8cda977a
xjanus12x <albertovillacarlos07@gmail.com>
2025-04-04 20:42:04 +0800

    add typed props to FeatureCard component

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -1,20 +1,27 @@
-import online from "../assets/icons/icon-online.svg.svg";
+type FeatureCardProps = {
+  title: string;
+  description: string;
+  icon: string;
+};
 
-export default function FeatureCard() {
+export default function FeatureCard({
+  title,
+  description,
+  icon,
+}: FeatureCardProps) {
   return (
     <article className="font-light max-w-[21.375rem] grid justify-items-center text-center px-[1.375rem] py-[1.125rem]">
       <img
         className="size-[4.5rem] rounded-full mb-[2.063rem]"
-        src={online}
+        src={icon}
         alt=""
         aria-hidden
       />
       <h2 className="mb-4 text-xl leading-[1.438em] text-card-title-foreground">
-        Fast Onboarding
+        {title}
       </h2>
       <p className="text-sm leading-[1.313em] text-card-description-foreground">
-        We don’t do branches. Open your account in minutes online and start
-        taking control of your finances right away.
+        {description}
       </p>
     </article>
   );
