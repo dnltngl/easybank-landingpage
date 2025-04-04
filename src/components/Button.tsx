de76cc2cd0c806e3dbfacaae1ee40c61d574e184
xjanus12x <albertovillacarlos07@gmail.com>
2025-04-04 21:11:18 +0800

    add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+export default function Button() {
+  return (
+    <button className="px-[2.188rem] py-[0.813rem] bg-button-gradient rounded-[3.125rem] text-sm leading-[1.138em] text-white">
+      Request Invite
+    </button>
+  );
+}
