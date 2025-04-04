5cac16324c1ef1de10a571eb5b841eaf12ea75b8
xjanus12x <albertovillacarlos07@gmail.com>
2025-04-04 21:10:44 +0800

    add button gradient in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "button-gradient":
+          "linear-gradient(to right, var(--teal) 0%, var(--green) 100%)",
+      },
       colors: {
         "card-author-foreground": "var(--gray-light)",
         "card-title-foreground": "var(--blue-dark)",
