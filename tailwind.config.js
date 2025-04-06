c302e570b42cd8ddc860aa467a0c70d0d76ba70e
xjanus12x <albertovillacarlos07@gmail.com>
2025-04-06 22:54:21 +0800

    add menu-foreground for mobile menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
           "linear-gradient(to right, var(--teal) 0%, var(--green) 100%)",
       },
       colors: {
+        "menu-foreground": "var(--gray-light)",
         "card-author-foreground": "var(--gray-light)",
         "card-title-foreground": "var(--blue-dark)",
         "card-description-foreground": "var(--gray-light)",
