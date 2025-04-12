c04df76b7357b369b3d85382c598bee8cf7fd962
xjanus12x <albertovillacarlos07@gmail.com>
2025-04-12 20:16:50 +0800

    add useInnerWidth hook to track window width on resize

---
+++ src/custom-hook/useInnerWidth.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+export function useInnerWidth() {
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setInnerWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return innerWidth;
+}
