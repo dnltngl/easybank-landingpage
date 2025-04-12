7e5cc45144b4b471272167ca90ca25bd9ab3cfaa
xjanus12x <albertovillacarlos07@gmail.com>
2025-04-12 20:14:29 +0800

    add className props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,12 @@
-export default function Button() {
+type ButtonProps = { className?: string };
+
+export default function Button({ className }: ButtonProps) {
   return (
-    <button className="px-[2.188rem] py-[0.813rem] bg-button-gradient rounded-[3.125rem] text-sm leading-[1.138em] text-white">
+    <button
+      className={`px-[2.188rem] py-[0.813rem] bg-button-gradient rounded-[3.125rem] text-sm leading-[1.138em] text-white ${
+        className || ""
+      }`}
+    >
       Request Invite
     </button>
   );
